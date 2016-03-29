0c2e2144ca85c0426279afb3b546942946440df0
Simon Olofsson <s.olofsson@gmail.com>
2016-03-29 18:38:56 +0200

    Removed TextUI and simplified the ValueComparator algorithm.

--- source/compare/ValueComparator.java
+++ source/compare/ValueComparator.java
@@ -7,19 +7,7 @@ public class ValueComparator implements Comparator<Valuable> {
 
 	public int compare(Valuable valuable1, Valuable valuable2) {
 
-		if(valuable1.calculateValueWithTax() > valuable2.calculateValueWithTax()) {
-
-			return -1;
-
-		} else if(valuable1.calculateValueWithTax() == valuable2.calculateValueWithTax()) {
-
-			return 0;
-
-		} else {
-
-			return 1;
-
-		}
+		return (int) (valuable2.calculateValueWithTax() - valuable1.calculateValueWithTax()); 
 
 	}
 
