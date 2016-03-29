ade040cde45e260639163da7626611847784ba11
Simon Olofsson <s.olofsson@gmail.com>
2016-03-29 18:06:06 +0200

    Removed RegistryView and duplicate Observer and Subject interfaces.

--- source/observer/Observer.java
+++
@@ -1,9 +0,0 @@
-package observer;
-
-public interface Observer {
-
-	void registerWithSubject(Observer observer);
-
-	void update();
-
-}
--- source/observer/Subject.java
+++
@@ -1,9 +0,0 @@
-package observer;
-
-public interface Subject {
-
-	void addObserver(Observer observer);
-
-	void updateObservers();
-
-}
