4f77d1c91759c5fb58963eb20e3212a128285e05
Simon Olofsson <s.olofsson@gmail.com>
2016-04-07 12:29:19 +0200

    Now tracking jPanels package.

---
+++ source/jPanels/ControlPanel.java
@@ -0,0 +1,28 @@
+package jPanels;
+
+import mvc.RegistryView;
+import javax.swing.*;
+
+public class ControlPanel extends JPanel {
+
+	public ControlPanel(RegistryView parentFrame) {
+
+		JLabel newValuable = new JLabel("New:");
+		add(newValuable);
+
+		String[] valuableCategories = {"Jewellry", "Stock", "Apparatus"};
+		JComboBox comboBox = new JComboBox(valuableCategories);
+		comboBox.addActionListener( event -> parentFrame.displayValuableDialog(this, (String) comboBox.getSelectedItem()) );
+		add(comboBox);
+
+		JButton show = new JButton("Show");
+		show.addActionListener(event -> parentFrame.showValuables());
+		add(show);
+
+		JButton crash = new JButton("Stock market crash");
+		crash.addActionListener(event -> parentFrame.setSharePricesToZero());
+		add(crash);
+
+	}
+
+}
---
+++ source/jPanels/SortingPanel.java
@@ -0,0 +1,47 @@
+package jPanels;
+
+import mvc.RegistryView;
+import compare.*;
+
+import java.util.Comparator;
+import javax.swing.*;
+import java.awt.*;
+
+public class SortingPanel extends JPanel {
+
+	private JRadioButton sortByName;
+
+	public SortingPanel(RegistryView parentFrame) {
+
+		setLayout(new BorderLayout());
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+		JLabel sorting = new JLabel("Sorting");
+		panel.add(sorting);
+
+		sortByName = new JRadioButton("Name");
+		panel.add(sortByName);
+		sortByName.addActionListener(event -> parentFrame.showValuables(getSelectedComparator()));
+		sortByName.setSelected(true);
+
+		JRadioButton sortByValue = new JRadioButton("Value");
+		panel.add(sortByValue);
+		sortByValue.addActionListener(event -> parentFrame.showValuables(getSelectedComparator()));
+
+		ButtonGroup buttonGroup = new ButtonGroup();
+		buttonGroup.add(sortByName);
+		buttonGroup.add(sortByValue);
+
+		add(panel, BorderLayout.SOUTH);
+
+	}
+
+	public Comparator getSelectedComparator() {
+
+		return sortByName.isSelected() ? new NameComparator() : new ValueComparator();
+
+	}
+
+}
