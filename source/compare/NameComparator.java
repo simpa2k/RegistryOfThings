204df09f0aacd09dbad147949867678c6c9f266c
Simon Olofsson <s.olofsson@gmail.com>
2016-04-08 13:08:20 +0200

    Added @Override-annotations to Comparator classes.

--- source/compare/NameComparator.java
+++ source/compare/NameComparator.java
@@ -5,6 +5,7 @@ import valuables.Valuable;
 
 public class NameComparator implements Comparator<Valuable> {
 
+	@Override
 	public int compare(Valuable valuable1, Valuable valuable2) {
 
 		return valuable1.getName().compareTo(valuable2.getName());
--- source/compare/ValueComparator.java
+++ source/compare/ValueComparator.java
@@ -5,6 +5,7 @@ import valuables.Valuable;
 
 public class ValueComparator implements Comparator<Valuable> {
 
+	@Override
 	public int compare(Valuable valuable1, Valuable valuable2) {
 
 		return (int) (valuable2.calculateValueWithTax() - valuable1.calculateValueWithTax()); 
